85db03ee540af29e092cb6ef2edd0a8ad71f4bdf
Sascha Debatin <s.debatin@hotmail.de>
2024-08-16 15:42:10 +0200

    rails watch list

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :lists
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
   root 'lists#index'
 end
